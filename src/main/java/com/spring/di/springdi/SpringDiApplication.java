06c187c8d682bb2dfa0d0d9122b66410ab79780a
Ali Hussein <ali.hussein@modernizingmedicine.com>
2021-03-15 13:04:51 -0400

    simple message

--- src/main/java/com/spring/di/springdi/SpringDiApplication.java
+++ src/main/java/com/spring/di/springdi/SpringDiApplication.java
@@ -1,13 +1,21 @@
 package com.spring.di.springdi;
 
+import com.spring.di.springdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String message = myController.sayHello();
+
+		System.out.println(message);
 	}
 
 }
---
+++ src/main/java/com/spring/di/springdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.spring.di.springdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello () {
+        System.out.println("Hello World");
+
+        return "hello guys!";
+    }
+}
